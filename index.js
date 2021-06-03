8c16f6267f8fa7ecc425895728311e534bcfa6c0
Rodion <rusyaloveee@gmail.com>
2021-06-03 12:22:14 +0300

    update set/remove methods

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ function AdvStorage() {
   let storage = _getStorage();
 
   function set(key, value) {
-    localStorage.setItem(key, value);
-    return _getStorage();
+    if (!!this.storage.find((item) => item.key === key)) {
+      const idx = this.storage.findIndex((item) => item.key === key);
+      this.storage[idx] = { key, value };
+      return this.storage;
+    } else {
+      localStorage.setItem(key, value);
+      const resp = storage.push({ key, value });
+
+      if (resp) {
+        return this.storage;
+      }
+    }
   }
 
   function get(key) {
@@ -11,11 +21,14 @@ function AdvStorage() {
   }
 
   function getAll() {
-    return _getStorage();
+    return this.storage;
   }
 
   function remove(key) {
     localStorage.removeItem(key);
+    const idx = this.storage.findIndex((item) => item.key === key);
+    this.storage.splice(idx, 1);
+    return this.storage;
   }
 
   function _getStorage() {
