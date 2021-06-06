5b7f2efb7ff32fcef4935ed853386a235f647c8a
Rodion <rusyaloveee@gmail.com>
2021-06-06 22:49:54 +0300

    Create advs ES6 class variant

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function AdvStorage() {
       this.storage[idx] = { key, value };
       return this.storage;
     } else {
-      localStorage.setItem(key, value);
       const resp = storage.push({ key, value });
 
       if (resp) {
@@ -47,4 +46,43 @@ function AdvStorage() {
   };
 }
 
-const advs = AdvStorage();
+class ADVStorage {
+  storage = [];
+  constructor() {
+    this._initStorage();
+  }
+
+  set(key, value) {
+    const idx = this.storage.findIndex((item) => item.key === key);
+
+    if (idx >= 0) {
+      this.storage[idx] = { key, value };
+    } else {
+      this.storage.push({ key, value });
+    }
+
+    localStorage.setItem(key, value);
+
+    return this.storage;
+  }
+  get(key) {
+    return localStorage.getItem(key);
+  }
+  getAll() {
+    return this.storage;
+  }
+  remove(key) {
+    localStorage.removeItem(key);
+    const idx = this.storage.findIndex((item) => item.key === key);
+    this.storage.splice(idx, 1);
+    return this.storage;
+  }
+  _initStorage() {
+    this.storage = Object.keys(localStorage).map((key) => {
+      const value = localStorage.getItem(key);
+      return { key, value };
+    });
+  }
+}
+
+const advs = new ADVStorage();
